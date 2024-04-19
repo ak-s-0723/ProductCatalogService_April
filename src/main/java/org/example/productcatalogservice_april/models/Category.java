57f67486301d3fc9a75a68c378f5060885117b5d
khannaanurag1 <khannaanurag1@gmail.com>
2024-04-19 09:30:10 +0530

    Setting up JPA and defining Entities

--- src/main/java/org/example/productcatalogservice_april/models/Category.java
+++ src/main/java/org/example/productcatalogservice_april/models/Category.java
@@ -1,6 +1,9 @@
 package org.example.productcatalogservice_april.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +11,13 @@ import java.util.List;
 
 @Setter
 @Getter
+@Entity
 public class Category extends BaseModel {
     private String name;
 
     private String description;
 
+    //mappedBy , because we want JPA to consider this relation once only
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
--- src/main/java/org/example/productcatalogservice_april/models/Product.java
+++ src/main/java/org/example/productcatalogservice_april/models/Product.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
+@Entity
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product extends BaseModel {
     private String name;
@@ -17,7 +19,7 @@ public class Product extends BaseModel {
     private String imageUrl;
 
     private Double price;
-
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
    // private boolean isPrimeSaleSpecific;
